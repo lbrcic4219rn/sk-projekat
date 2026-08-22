d9d7dc7f984ee8a534019fdaf0675fbc4009c41d
lukabrcic <lukabrcic.lb@gmail.com>
2026-08-22 21:54:35 +0200

    Project reaorganization and modules creation

---
+++ hotel-reservation-service/src/test/java/com/sk/hotelreservationservice/HotelReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.hotelreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
